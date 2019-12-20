ce0d65cc05e666cf534c16e1d4da51509ef42b81
tikenov <azamatkz90@gmail.com>
2019-12-20 08:35:14 +0300

    routes added

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
-const PORT = process.env.PORT || 5000;
+
+connectDB();
+
 
 app.get('/', (req, res) => {
     res.send("Hey, app is running");
 });
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`App running on port ${PORT}`));
